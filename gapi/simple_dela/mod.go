26f0bfb8983dcc66179b017b214e66d833e2577a
Noémien Kocher <nkcr.je@gmail.com>
2022-02-15 12:17:02 +0100

    Adds start/stop functionalities to simple_dela

--- gapi/simple_dela/mod.go
+++ gapi/simple_dela/mod.go
@@ -12,17 +12,21 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
 	accessContract "go.dedis.ch/dela/contracts/access"
+	"go.dedis.ch/dela/core"
 	"go.dedis.ch/dela/core/txn"
 	"go.dedis.ch/dela/core/txn/signed"
 	"go.dedis.ch/dela/crypto/bls"
 	"go.dedis.ch/dela/crypto/loader"
+	"go.dedis.ch/dela/mino"
 	"go.dedis.ch/dela/mino/minogrpc"
 	"go.dedis.ch/dela/mino/minogrpc/session"
+	"go.dedis.ch/dela/mino/router"
 	"golang.org/x/xerrors"
 
 	delapkg "go.dedis.ch/dela"
@@ -57,8 +61,12 @@ func main() {
 
 	nodes := make([]dela, n)
 
+	nodesPortStart := 5000
+	proxyPortStart := 4000
+
 	for i := 0; i < n; i++ {
-		node, err := newDelaNode(filepath.Join(dir, fmt.Sprintf("node%d", i)), 0)
+		node, err := newDelaNode(filepath.Join(dir, fmt.Sprintf("node%d", i)),
+			nodesPortStart+i)
 		if err != nil {
 			panic("failed to create: " + err.Error())
 		}
@@ -79,12 +87,18 @@ func main() {
 			panic(fmt.Sprintf("mino not watchable: %T", node.GetMino()))
 		}
 
-		config := fmt.Sprintf("\t{\"id\": \"%s\", \"addr\": \"%s\", \"proxy\": \"http://127.0.0.1:%04d\"}", getID(i), mino.GetAddress().String(), 4000+i)
+		config := fmt.Sprintf("\t{\"id\": \"%s\", \"addr\": \"%s\", \"proxy\": "+
+			"\"http://127.0.0.1:%04d\"}", getID(i), mino.GetAddress().String(),
+			proxyPortStart+i)
 		nodeConfigs[i] = config
 
 		go func(i int, mino *minogrpc.Minogrpc) {
+			dn := newDynamicNode(mino.GetAddress().String(), nodesPortStart+i,
+				nodes[i], filepath.Join(dir, fmt.Sprintf("node%d", i)), stop)
+
 			fmt.Printf("Server %d, addr: %s\n", i, mino.GetAddress().String())
-			listen(mino, fmt.Sprintf(":%04d", 4000+i), stop)
+
+			listen(dn)
 		}(i, mino)
 	}
 
@@ -212,10 +226,140 @@ func main() {
 	fmt.Println("done, bye 👋")
 }
 
-func listen(minogrpc *minogrpc.Minogrpc, addr string, stop chan struct{}) {
-	srv := delahttp.NewHTTP(addr)
+// newDynamicNode returns a new initialized dynamic nodes
+func newDynamicNode(addr string, port int, node dela, confPath string,
+	globalstop chan struct{}) dynamicNode {
 
-	watcher := minogrpc.GetTrafficWatcher()
+	dn := dynamicNode{
+		addr:       addr,
+		port:       port,
+		node:       node,
+		configPath: confPath,
+
+		globalstop:  globalstop,
+		sessionstop: make(chan struct{}),
+
+		inWatcher:  core.NewWatcher(),
+		outWatcher: core.NewWatcher(),
+	}
+
+	dn.watchPackets()
+
+	return dn
+}
+
+// event redefines the internal.Event type from dela. We can't directly use it
+// because it is in the internal/ package.
+type event struct {
+	address mino.Address
+	pkt     router.Packet
+}
+
+// dynamicNode wraps a dela node that can be stopped et re-started
+type dynamicNode struct {
+	addr       string
+	port       int
+	node       dela
+	configPath string
+
+	globalstop  chan struct{}
+	sessionstop chan struct{}
+
+	// we use intermediary watchers because if we used the dela watcher, they
+	// would close when the node stops, but Polypus still wants to read from it
+	// when the node restarts.
+	inWatcher  core.Observable
+	outWatcher core.Observable
+}
+
+// start starts the dela node if not already started
+func (dn *dynamicNode) start() {
+	if dn.node != nil {
+		fmt.Println("ERROR: node already started", dn.port)
+		return
+	}
+
+	dn.sessionstop = make(chan struct{})
+
+	fmt.Printf("starting :%d with config from %s\n", dn.port, dn.configPath)
+
+	node, err := newDelaNode(dn.configPath, dn.port)
+	if err != nil {
+		panic("failed to create: " + err.Error())
+	}
+
+	dn.node = node
+
+	dn.watchPackets()
+}
+
+// stop stops the dela node. It won't close the ins/outs channel opened by
+// Polypus.
+func (dn *dynamicNode) stop() {
+	fmt.Println("stopping", dn.addr)
+
+	if dn.node == nil {
+		fmt.Println("ERROR: node already stopped")
+		return
+	}
+
+	close(dn.sessionstop)
+	dn.node.Stop()
+	dn.node = nil
+}
+
+// watchPackets will notify the dynamicNode watchers when packets are
+// sent/received from the dela node. This function must be called each time the
+// dela node is (re)started.
+func (dn *dynamicNode) watchPackets() {
+	mino, ok := dn.node.GetMino().(*minogrpc.Minogrpc)
+	if !ok {
+		panic("not minogrpc")
+	}
+
+	watcher := mino.GetTrafficWatcher()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-dn.sessionstop
+		cancel()
+	}()
+
+	go func() {
+		ins := watcher.WatchIns(ctx)
+
+		for pkt := range ins {
+			dn.inWatcher.Notify(event{
+				address: pkt.Address,
+				pkt:     pkt.Pkt,
+			})
+		}
+	}()
+
+	go func() {
+		outs := watcher.WatchOuts(ctx)
+
+		for pkt := range outs {
+			dn.outWatcher.Notify(event{
+				address: pkt.Address,
+				pkt:     pkt.Pkt,
+			})
+		}
+	}()
+}
+
+func (dn *dynamicNode) watchIns(ctx context.Context) <-chan event {
+	return watch(ctx, dn.inWatcher)
+}
+
+func (dn *dynamicNode) watchOuts(ctx context.Context) <-chan event {
+	return watch(ctx, dn.outWatcher)
+}
+
+// listen starts the dela proxy service that Polypus needs
+func listen(dn dynamicNode) {
+	srv := delahttp.NewHTTP(":" + strconv.Itoa(dn.port-1000))
 
 	srv.RegisterHandler("/recv", func(w http.ResponseWriter, r *http.Request) {
 		flusher, ok := w.(http.Flusher)
@@ -230,15 +374,16 @@ func listen(minogrpc *minogrpc.Minogrpc, addr string, stop chan struct{}) {
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		ins := watcher.WatchIns(ctx)
+		ins := dn.watchIns(r.Context())
 
 		for {
 			select {
 			case in := <-ins:
-				message := fmt.Sprintf(dataRecv, "*not used*", in.Address.(session.Address).GetDialAddress(), time.Now().UnixMicro(), in.Pkt.GetPacketID())
+				addr := in.address.(session.Address).GetDialAddress()
+				tt := time.Now().UnixMicro()
+
+				message := fmt.Sprintf(dataRecv, "*not used*", addr, tt, in.pkt.GetPacketID())
+
 				fmt.Fprintf(w, message)
 				flusher.Flush()
 			case <-r.Context().Done():
@@ -260,17 +405,16 @@ func listen(minogrpc *minogrpc.Minogrpc, addr string, stop chan struct{}) {
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		outs := watcher.WatchOuts(ctx)
+		outs := dn.watchOuts(r.Context())
 
 		for {
 			select {
 			case out := <-outs:
-				msgContent := out.Pkt.GetMessage()
+				msgContent := out.pkt.GetMessage()
+				addr := out.address.(session.Address).GetDialAddress()
+				tt := time.Now().UnixMicro()
 
-				message := fmt.Sprintf(dataSent, msgContent, out.Address.(session.Address).GetDialAddress(), time.Now().UnixMicro(), out.Pkt.GetPacketID())
+				message := fmt.Sprintf(dataSent, msgContent, addr, tt, out.pkt.GetPacketID())
 				fmt.Fprintf(w, message)
 
 				flusher.Flush()
@@ -280,13 +424,22 @@ func listen(minogrpc *minogrpc.Minogrpc, addr string, stop chan struct{}) {
 		}
 	})
 
+	srv.RegisterHandler("/start", func(rw http.ResponseWriter, r *http.Request) {
+		dn.start()
+
+	})
+
+	srv.RegisterHandler("/stop", func(rw http.ResponseWriter, r *http.Request) {
+		dn.stop()
+	})
+
 	go srv.Listen()
 
 	time.Sleep(time.Second)
 
 	fmt.Println("server listening on", srv.GetAddr())
 
-	<-stop
+	<-dn.globalstop
 	srv.Stop()
 }
 
@@ -343,4 +496,36 @@ func getID(i int) string {
 	secondLetter := byte('A') + byte(i%26)
 
 	return string(firstLetter) + string(secondLetter)
+}
+
+// observer defines an observer to fill a channel
+//
+// - implements core.Observer
+type observer struct {
+	ch chan event
+}
+
+// NotifyCallback implements core.Observer. It drops the message if the channel
+// is full.
+func (o observer) NotifyCallback(e interface{}) {
+	select {
+	case o.ch <- e.(event):
+	default:
+		fmt.Println("channel full")
+	}
+}
+
+// watch implements core.Observer
+func watch(ctx context.Context, watcher core.Observable) <-chan event {
+	obs := observer{ch: make(chan event, 10)}
+
+	watcher.Add(obs)
+
+	go func() {
+		<-ctx.Done()
+		watcher.Remove(obs)
+		close(obs.ch)
+	}()
+
+	return obs.ch
 }
